634baa8b108e804d4bff3b02892bd5e96e98a555
LukashevichEvgeniy <humansyndr8me@gmail.com>
2023-01-10 13:08:38 +0800

    the 3rd homework added

--- src/seminar03_hw/Program.java
+++ src/seminar03_hw/Program.java
@@ -17,38 +17,7 @@ public class Program {
 
         doc1.setFormat(new NewFormat());
         doc1.click();
-
-        // #region
-        // Document doc = new Document();
-        // doc.setWorker(new Worker(22, 221, "Какое-то имя"));
-
-        // doc.SaveAs();// Save html
-        // // xml
-        // // <xml>
-        // // <Worker>
-        // // <age>22</age>
-        // // <salary>221</salary>
-        // // <name>Какое-то имя</name>
-        // // </Worker>
-        // // </xml>
-
-        // // md
-        // doc.SaveAs();// Save md
-        // // # Worker
-        // // * age 22
-        // // * salary 221
-        // // * name Какое-то имя
-
-        // // json
-        // doc.SaveAs();// Save json
-        // // {
-        // // "worker" : {
-        // // "age":22,
-        // // "salary":221,
-        // // "name":"Какое-то имя",
-        // // }
-        // // }
-        // #endregion
+        
     }
 }
 
